b5d1095d206fbb035678acb339eaec1e22a9a2aa
Nguyen Huu Den <dennh@fsoft.com.vn>
2016-12-20 10:40:23 +0700

    Update drag menu search

--- MapLife/MapLife/MenuSearchDraggingButton.m
+++ MapLife/MapLife/MenuSearchDraggingButton.m
@@ -8,8 +8,8 @@
 
 #import "MenuSearchDraggingButton.h"
 
-CGFloat const MaximumDraggingButton = 150;
-CGFloat const MinimumDraggingBUtton = 30;
+CGFloat const MaximumDraggingButton = 400;
+CGFloat const MinimumDraggingBUtton = 80;
 /**
  *  Image dragging button menu search
  */
@@ -66,6 +66,7 @@ const UIEdgeInsets MenuInsetsBackground = {30, 30, 5, 5};
                                resizableImageWithCapInsets:MenuInsetsBackground]
                   forState:UIControlStateNormal];
   }
+    [self setNeedsLayout];
 }
 /**
  *  Touch cancel button dragging
@@ -87,69 +88,64 @@ const UIEdgeInsets MenuInsetsBackground = {30, 30, 5, 5};
  *  @param event   Event touch
  */
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-  if (!self.disableDragging) {
-
-    UITouch *touchEnd = [touches anyObject];
-    self.pointEndDragg = [touchEnd locationInView:self.window];
-
-    CGFloat xDistance = self.pointEndDragg.x - self.pointBeginDragg.x;
-    CGFloat yDistance = self.pointEndDragg.y - self.pointBeginDragg.y;
-    CGFloat distance = sqrtf((xDistance * xDistance) + (yDistance * yDistance));
-
-    if (self.pointBeginDragg.y > self.pointEndDragg.y) {
-
-      if ((self.superview.frame.size.height + distance) >
-          ([UIScreen mainScreen].bounds.size.height - MaximumDraggingButton)) {
-
-        distance = [UIScreen mainScreen].bounds.size.height -
-                   self.superview.frame.size.height - MaximumDraggingButton;
-      }
-      if ((self.superview.frame.size.height + distance) <=
-          ([UIScreen mainScreen].bounds.size.height - MaximumDraggingButton)) {
-
-        CGRect expectRect =
-            CGRectMake(self.superview.frame.origin.x,
-                       self.superview.frame.origin.y - distance,
-                       self.superview.frame.size.width,
-                       self.superview.frame.size.height + distance);
-        if ([self.delegate respondsToSelector:@selector(didMoveToDistance:
-                                                         orirentationIsUp:
-                                                                 withView:
-                                                                  andRect:)]) {
-          [self.delegate didMoveToDistance:distance
-                          orirentationIsUp:NO
-                                  withView:self.superview
-                                   andRect:expectRect];
+    
+    // Enable drag
+    if (!self.disableDragging)
+    {
+        UITouch* touchMove = [touches anyObject];
+        self.pointEndDragg = [touchMove locationInView:self.window];
+        
+        CGFloat xDist = (self.pointEndDragg.x - self.pointBeginDragg.x);
+        CGFloat yDist = (self.pointEndDragg.y - self.pointBeginDragg.y);
+        CGFloat distance = sqrt((xDist * xDist) + (yDist * yDist));
+        
+        if (self.pointBeginDragg.y > self.pointEndDragg.y)
+        {
+            if ((self.superview.frame.size.height + distance) >
+                ([UIScreen mainScreen].bounds.size.height - MaximumDraggingButton))
+            {
+                distance = [UIScreen mainScreen].bounds.size.height - self.superview.frame.size.height -
+                MaximumDraggingButton;
+            }
+            if ((self.superview.frame.size.height + distance) <=
+                ([UIScreen mainScreen].bounds.size.height - MaximumDraggingButton))
+            {
+                CGRect expectRect =
+                CGRectMake(self.superview.frame.origin.x, self.superview.frame.origin.y - distance,
+                           self.superview.frame.size.width, self.superview.frame.size.height + distance);
+                
+                if ([self.delegate respondsToSelector:@selector(didMoveToDistance:orirentationIsUp:withView:andRect:)])
+                {
+                    [self.delegate didMoveToDistance:distance
+                                     orirentationIsUp:NO
+                                            withView:self.superview
+                                             andRect:expectRect];
+                }
+            }
         }
-      }
-    } else {
-      if ((self.superview.frame.size.height + distance) >
-          MinimumDraggingBUtton) {
-
-        distance = MinimumDraggingBUtton - self.superview.frame.size.height;
-      }
-      if ((self.superview.frame.size.height - distance) >=
-          MinimumDraggingBUtton) {
-
-        CGRect expectRect =
-            CGRectMake(self.superview.frame.origin.x,
-                       self.superview.frame.origin.y + distance,
-                       self.superview.frame.size.width,
-                       self.superview.frame.size.height - distance);
-        if ([self.delegate respondsToSelector:@selector(didMoveToDistance:
-                                                         orirentationIsUp:
-                                                                 withView:
-                                                                  andRect:)]) {
-
-          [self.delegate didMoveToDistance:distance
-                          orirentationIsUp:YES
-                                  withView:self.superview
-                                   andRect:expectRect];
+        else
+        {
+            if ((self.superview.frame.size.height + distance) < MinimumDraggingBUtton)
+            {
+                distance = MinimumDraggingBUtton - self.superview.frame.size.height;
+            }
+            if ((self.superview.frame.size.height - distance) >= MinimumDraggingBUtton)
+            {
+                CGRect expectRect =
+                CGRectMake(self.superview.frame.origin.x, self.superview.frame.origin.y + distance,
+                           self.superview.frame.size.width, self.superview.frame.size.height - distance);
+    
+                if ([self.delegate respondsToSelector:@selector(didMoveToDistance:orirentationIsUp:withView:andRect:)])
+                {
+                    [self.delegate didMoveToDistance:distance
+                                     orirentationIsUp:YES
+                                            withView:self.superview
+                                             andRect:expectRect];
+                }
+            }
         }
-      }
+        self.pointBeginDragg = self.pointEndDragg;
     }
-    self.pointBeginDragg = self.pointEndDragg;
-  }
 }
 /**
  *  Touch end
